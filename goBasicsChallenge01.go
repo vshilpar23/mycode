491c4d957a47787bf8851c8766c2623c876b5778
vshilpar23 <vshilpar23@gmail.com>
2022-10-31 17:20:25 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Main function
+func main() {
+	const name, city, state = "Shilpa", "Dallas", "Texas"
+	fmt.Println("Name is ", name)
+	fmt.Println("City: ", city, ",", "State:", state)
+}
